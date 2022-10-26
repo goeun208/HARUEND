d0338cd4113bf344d2f5ce5fa91587100d3ae6a7
goeun208 <qop341@gmail.com>
2022-10-27 02:40:04 +0900

    FEAT: first firebase setting

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,9 +6,19 @@ const Header = () => {
   
   return (
     <div className={styles.wrap}>
-      <Link to="/">home</Link>
-      <Link to="/write">write</Link>
-      <Link to="/list">list</Link>
+      <div className={styles.top}></div>
+      <div className={styles.box}>
+        <Link to="/" className={styles.logo}>HARUEND</Link>
+        <div className={styles.catebox}>
+          <Link to="/" className={styles.category}>home</Link>
+          <Link to="/write" className={styles.category}>write</Link>
+          <Link to="/list" className={styles.category}>list</Link>
+        </div>
+        <div className={styles.logbox}>
+          <Link to="/write" className={styles.category2}>Log In</Link>
+          <Link to="/list" className={styles.category2}>Sign Up</Link>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/format/weather.ts
@@ -0,0 +1,4 @@
+export const weather = () => {
+  let weather:string = "맑음";
+  return weather;
+}
